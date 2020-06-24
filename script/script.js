aa07549894a71c6446f78ace6eaab4445c0081d7
chages16 <chages16@gmail.com>
2020-06-24 22:04:51 +0930

    Code comments added

--- script/script.js
+++ script/script.js
@@ -1,3 +1,4 @@
+//List of global varials
 var queryURL = "";
 var city = "";
 var icons = "http://openweathermap.org/img/wn/"
@@ -5,21 +6,19 @@ var citySave = [];
 var listOfIds = ["Day1", "Day2", "Day3", "Day4", "Day5"]
 var listOfIds2 = ["#Day1", "#Day2", "#Day3", "#Day4", "#Day5"]
 
-
-
+//Function to reduce temperatures to 2 decimal points
 function round(value, decimals) {
     return Number(Math.round(value + 'e' + decimals) + 'e-' + decimals);
 }
 
+//click function for list items
 $(".cityList").on("click", function (event) {
-    console.log("nkrnfrkjnnj");
     city = event.target.textContent;
-    console.log(city)
     queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=393019def5a41c16a42cb577477e0a0d";
     getData();
-
 })
 
+//click function for button, saves input data to list and stored data
 $(".add-city").on("click", function (event) {
     event.preventDefault();
     city = $("#city-input").val().trim();
@@ -30,24 +29,21 @@ $(".add-city").on("click", function (event) {
     $("#city-input").val("");
     var listItem = $("<li>");
     listItem.text(city)
-    console.log(listItem);
     $(".cityList").prepend(listItem);
     citySave.push(city);
     localStorage.setItem("citySave", JSON.stringify(citySave));
-    console.log("hi");
     queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=393019def5a41c16a42cb577477e0a0d"
-    console.log(queryURL);
     getData();
 })
 
+//Get Data function
 function getData() {
+    //Ajax function to get current weather data and longtitude and latitude
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function (response) {
-        console.log(response);
-        console.log(response.name)
-        // Storing the rating data
+        // Variables for collected data
         var cityName = response.name;
         var cityTemp = round(response.main.temp - 273.15, 2);
         var cityHumidity = response.main.humidity;
@@ -56,28 +52,18 @@ function getData() {
         var cityLon = response.coord.lon;
         var cityIcon = response.weather[0].icon;
         var cityCountry = response.sys.country;
-        console.log(cityCountry)
-        console.log(cityIcon);
-        console.log(cityName);
-        console.log(cityTemp);
-        console.log(cityHumidity);
-        console.log(cityWindSpeed);
-        console.log(cityLat);
-        console.log(cityLon);
         var queryUV = "https://api.openweathermap.org/data/2.5/uvi?appid=393019def5a41c16a42cb577477e0a0d&lat=" + cityLat + "&lon=" + cityLon;
+        //Ajax function to get current UV Index with longtitude and latitude
         $.ajax({
             url: queryUV,
             method: "GET"
         }).then(function (response2) {
-            console.log(response2);
-            console.log(queryUV)
             var cityUV = response2.value;
-            console.log(cityUV);
+            //Show all data on page
             $(".weather-points").empty();
             $(".weather-forecast").empty();
             var h1One = $("<h2>").text(cityName + ", " + cityCountry + ", " + moment().format("DD/MM/YYYY"));
             var imgicon = $("<img>").attr("src", icons + cityIcon + "@2x.png").css("float", "right").css("clear", "left");
-            console.log(imgicon);
             var h4One = $("<h4>").text("Temperature: " + cityTemp + " °C");
             var h4Two = $("<h4>").text("Humidity: " + cityHumidity + "%");
             var h4Three = $("<h4>").text("Wind Speed: " + cityWindSpeed + " MPH");
@@ -92,21 +78,17 @@ function getData() {
             else {
                 $(".uvindex").attr("class", "uvindexfav");
             }
+            //Using Ajax and latitude and longtitude to get forecast
             var queryForecast = "https://api.openweathermap.org/data/2.5/onecall?lat=" + cityLat + "&lon=" + cityLon + "&units=metric&exclude=hourly,current,minutely&appid=393019def5a41c16a42cb577477e0a0d";
             $.ajax({
                 url: queryForecast,
                 method: "GET"
             }).then(function (response3) {
-                console.log(response3);
-                console.log(queryForecast);
-                console.log(response3.daily[1].temp.max);
-                console.log(response3.daily[1].humidity);
-                console.log(response3.daily[1].weather[0].icon);
+                //display 5 day forecast on screen
                 for (var i = 1; i < 6; i++) {
                     var forecastTemp = (response3.daily[i].temp.max);
                     var forecastHumidity = (response3.daily[i].humidity);
                     var forecastIcon = response3.daily[i].weather[0].icon;
-                    console.log(queryForecast);
                     var newimgicon = $("<img>").attr("src", icons + forecastIcon + ".png");
                     var newDiv = $("<div>").attr("class", "forecast").attr("id", listOfIds[i - 1]).css("float", "left");
                     var pOne = $("<p>").text("Temp: " + forecastTemp + " °C");
@@ -120,52 +102,20 @@ function getData() {
     })
 }
 
+//Clear list function
 $(".cleary").on("click", function (event) {
     event.preventDefault();
     var confirmClear = confirm("Are you sure you want to clear all the data?")
     if (confirmClear) {
         $(".cityList").empty();
+        $(".weather-points").empty();
+        $(".weather-forecast").empty();
         citySave = [];
         localStorage.setItem("citySave", JSON.stringify(citySave));
     }
 })
 
-
-
-/*
-    // Displaying the rating
-    movieDiv.append(pOne);
-
-    // Storing the release year
-    var released = response.Released;
-
-    // Creating an element to hold the release year
-    var pTwo = $("<p>").text("Released: " + released);
-
-    // Displaying the release year
-    movieDiv.append(pTwo);
-
-    // Storing the plot
-    var plot = response.Plot;
-
-    // Creating an element to hold the plot
-    var pThree = $("<p>").text("Plot: " + plot);
-
-    // Appending the plot
-    movieDiv.append(pThree);
-
-    // Retrieving the URL for the image
-    var imgURL = response.Poster;
-
-    // Creating an element to hold the image
-    var image = $("<img>").attr("src", imgURL);
-
-    // Appending the image
-    movieDiv.append(image);
-
-    // Putting the entire movie above the previous movies
-    $("#movies-view").prepend(movieDiv);
-    */
+//Load Page Function, gets all the currently stored data
 
 loadPage();
 
@@ -174,11 +124,9 @@ function loadPage() {
     if (storedList !== null) {
         citySave = storedList
         for (var i = 0; i < citySave.length; i++) {
-            console.log("000");
             var listItem = $("<li>");
             listItem.text(citySave[i]);
             $(".cityList").prepend(listItem);
         }
     }
 }
-
